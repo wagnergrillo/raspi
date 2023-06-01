b0cd7b3beb440477675a4a7bcd4409c0faf0389e
wagnergrillo <135171225+wagnergrillo@users.noreply.github.com>
2023-06-01 10:10:04 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -4,5 +4,6 @@ git clone https://github.com/Lcdwiki/LCD-show
 chmod -R 755 LCD-show
 sudo mkdir /home/pi/.config/autostart
 sudo cp auto.desktop /home/pi/.config/autostart/auto.desktop
+sudo cp autostart /etc/xdg/lxsession/LXDE-pi/autostart
 cd LCD-show
 ./MHS35-show
