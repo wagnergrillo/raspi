25fa0e0ddfdb0f0d52a04d03ef09b87a8b35b424
wagnergrillo <135171225+wagnergrillo@users.noreply.github.com>
2023-06-09 14:02:53 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -5,5 +5,6 @@ chmod -R 755 LCD-show
 sudo mkdir /home/pi/.config/autostart
 sudo cp auto.desktop /home/pi/.config/autostart/auto.desktop
 sudo cp autostart /etc/xdg/lxsession/LXDE-pi/autostart
+sudo cp lightdm.conf /etc/lightdm
 cd LCD-show
 ./MHS35-show
