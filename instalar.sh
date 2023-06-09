d19b2cb21967eef538772bfbf6231e5e990eb05e
wagnergrillo <135171225+wagnergrillo@users.noreply.github.com>
2023-06-09 15:54:56 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -6,5 +6,7 @@ sudo mkdir /home/pi/.config/autostart
 sudo cp auto.desktop /home/pi/.config/autostart/auto.desktop
 sudo cp autostart /etc/xdg/lxsession/LXDE-pi/autostart
 sudo cp lightdm.conf /etc/lightdm
+sudo apt-get install unclutter
+unclutter -idle 0.01 -root
 cd LCD-show
 ./MHS35-show
