f533590482ca67c71ad4d97745136744803e3eb1
wagnergrillo <135171225+wagnergrillo@users.noreply.github.com>
2023-06-16 10:58:04 -0300

    Update ponto.py

--- ponto.py
+++ ponto.py
@@ -1,35 +1,13 @@
-# -*- coding: utf-8 -*-
 import requests
 import json
 import datetime
 import tkinter as tk
 
-def obter_dados(requisicao_numero):
-    url = "https://apex.pampili.com.br/ords/afvserver/rm/funcionarios?key=PontoPampili@)@!&coligada=1&filial=7"
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        #print(data)
-        funcionarios = data['items']
-        for funcionario in funcionarios:
-            cracha = funcionario.get('cracha')
-            nome = funcionario.get('nome')
-            if int(cracha) == int(requisicao_numero):
-                return nome
-    return None
-
-class PontoGUI(tk.Tk):   
+class PontoGUI(tk.Tk):
     def __init__(self):
-        super().__init__()  
+        super().__init__()
         self.attributes("-fullscreen", True)
-       # self.wm_attributes("-toolwindow", True)
         self.title("Coletor de Ponto")
-        
-        #self.margin_frame = tk.Frame(self)
-	#self.margin_frame.pack(pady=50)
 
         self.img = tk.PhotoImage(file="/home/pi/raspi/pam1.png")
         self.image_label = tk.Label(self, image=self.img)
@@ -37,11 +15,13 @@ class PontoGUI(tk.Tk):
         self.time_label = tk.Label(self, font=("Arial", 35))
         self.time_label.pack()
         self.update_time()
-        self.status_label = tk.Label(self, text="Aproxime o Chachá...", font=("Arial", 20), fg="#F400A1")
+        self.status_label = tk.Label(self, text="Aproxime o Chachá...", font=("Arial", 30), fg="#F400A1")
         self.status_label.pack(pady=20)
         self.numero = ""
         self.bind("<Key>", self.key_pressed)
 
+        self.funcionarios = {} # Var dados Apex
+        self.atualizar_dados_apex() # Obtém os dados do Apex inicialmente
 
     def update_time(self):
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
@@ -67,9 +47,22 @@ class PontoGUI(tk.Tk):
         except:
             return False
 
+    def atualizar_dados_apex(self):
+        if self.verificar_conexao_internet():
+            url = "https://apex.pampili.com.br/ords/afvserver/rm/funcionarios?key=PontoPampili@)@!&coligada=1&filial=7"
+            headers = {
+                'Content-Type': 'application/json'
+            }
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                data = response.json()
+                funcionarios = data['items']
+                self.funcionarios = {funcionario['cracha']: funcionario['nome'] for funcionario in funcionarios}
+                print("Dados do Apex atualizados.")
+        self.after(3600000, self.atualizar_dados_apex) # Atualiza os dados a cada 1 hora -> está em milisegundos
+
     def enviar_dados_apex_oracle(self):
         with open("/home/pi/raspi/dados.txt", "a") as arquivo:
-            
             horario = datetime.datetime.now().strftime(f'%d%m%y%H%M')
             print(horario)
             dados = f"{self.numero};{horario}\n"
@@ -80,7 +73,6 @@ class PontoGUI(tk.Tk):
                 dados = arquivo.readlines()
 
             url = 'https://apex.pampili.com.br/ords/afvserver/ponto/pontoparanaiba'
-            
             headers = {'Content-type': 'application/json'}
             linhas_mantidas = []
 
@@ -96,34 +88,36 @@ class PontoGUI(tk.Tk):
 
                 if response.status_code == 200:
                     print(f"Dado {linha.strip()} enviado com sucesso para o sistema no Apex Oracle.")
-                    x = 1
                 else:
-                    x = 0
                     print(f"Falha ao enviar dado {linha.strip()}. Status Code: {response.status_code}")
-                    linhas_mantidas.append(linha) 
+                    linhas_mantidas.append(linha)
 
             with open("/home/pi/raspi/dados.txt", "w") as arquivo:
                 arquivo.writelines(linhas_mantidas)
 
-            nome_funcionario = obter_dados(self.numero)
+            nome_funcionario = self.funcionarios.get(self.numero)
 
-            if nome_funcionario and x == 1:
+            if nome_funcionario:
                 self.status_label.config(text=f"{nome_funcionario}")
-            elif x == 1:
-                self.status_label.config(text="Cracha inexistente, ponto registrado!")
-            else: 
-                self.status_label.config(text="ERRO!")
+            else:
+                self.status_label.config(text="Crachá inexistente, ponto registrado!")
 
             self.after(1000, lambda: self.status_label.config(text="Aproxime o Chachá..."))
+        
         else:
-            self.status_label.config(text="Dados armazenados, sem internet.")
+            nome_funcionario = self.funcionarios.get(self.numero)
+            if nome_funcionario:
+                self.status_label.config(text=f"{nome_funcionario}")
+            else:
+                self.status_label.config(text="Crachá inexistente, ponto registrado!")
+            
             self.after(1000, lambda: self.status_label.config(text="Aproxime o Chachá..."))
 
         self.numero = ""
 
 if __name__ == "__main__":
     app = PontoGUI()
-    
+
     def verificar_conexao_e_enviar_dados():
         if app.verificar_conexao_internet():
             with open("/home/pi/raspi/dados.txt", "r") as arquivo:
@@ -131,7 +125,7 @@ if __name__ == "__main__":
 
             if dados:
                 url = 'https://apex.pampili.com.br/ords/afvserver/ponto/pontoparanaiba'
-                
+
                 headers = {'Content-type': 'application/json'}
                 linhas_mantidas = []
 
@@ -154,7 +148,7 @@ if __name__ == "__main__":
                 with open("/home/pi/raspi/dados.txt", "w") as arquivo:
                     arquivo.writelines(linhas_mantidas)
 
-        app.after(300000, verificar_conexao_e_enviar_dados)  # Executar aaa cada 5 minutos que sãoo 300000 milissegundos
+        app.after(300000, verificar_conexao_e_enviar_dados)
 
     verificar_conexao_e_enviar_dados()
     app.mainloop()
